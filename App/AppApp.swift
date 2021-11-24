cc5d5d64f9b791fa555b5c126062458561ee9412
Mateo Elosua <melosua@lcs.on.ca>
2021-11-24 11:31:21 -0500

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Mateo Elosua on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
